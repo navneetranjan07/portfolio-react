27f4b9d58174ee1bf83899a3fd00693d44aa8e92
navneetranjan07 <ridestatus07@gmail.com>
2024-04-15 23:46:28 +0530

    g

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,7 +24,7 @@ const Navbar = () => {
             </div>
 
             <div className={nav ? 'z-10 fixed h-full left-0 top-0 w-[60%] bg-[#202121] ease-in-out duration-500' : 'fixed left-[-100%]'}>
-                <h1 className='text-3xl primary-color m-4'>J. DOE</h1>
+                <h1 className='text-3xl primary-color m-4'>Navneet</h1>
                 <ul className='p-8 text-2xl'>
                     <li className='p-2'><a href="#about">About</a></li>
                     <li className='p-2'><a href="#work">Work</a></li>
